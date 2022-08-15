ccfbd3ec520f025c0de8efb72e7d2dccdd15cd44
Karl Keller <kgk86@hotmail.com>
2022-08-15 21:59:40 +0200

    Make #is_index? method private

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -14,8 +14,8 @@ defmodule Streamers do
     Enum.find Path.wildcard(files), is_index?(&1)
   end
 
-  def is_index?(file) do
-    File.open file, fn
+  defp is_index?(file) do
+    File.open! file, fn
       "#EXTM3U\n#EXT-X-STREAM-INF" <> _ -> true
       _ -> false
     end
